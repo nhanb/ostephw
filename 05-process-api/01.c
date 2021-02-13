7734e8b7304b5b9848eb3bc823af3ea6c75b2432
Bùi Thành Nhân <hi@imnhan.com>
2021-02-13 17:19:50 +0700

    baby's first c

--- 05-process-api/01.c
+++ 05-process-api/01.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main() {
-  printf("Hello\n");
-  return 1;
+    int a = 1;
+    printf("> a = %d\n", a);
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child
+        printf("> Child: a = %d\n", a);
+        a = 2;
+        printf("> Child: a = %d\n", a);
+    } else {
+        // parent
+        printf("> Parent: a = %d\n", a);
+        a = 3;
+        printf("> Parent: a = %d\n", a);
+    }
+    return 1;
 }
